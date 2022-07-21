6565b543bc752e929604cc4a8c664e231932cc99
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-07-21 14:48:31 +0530

    Add files via upload

---
+++ Implement Queue using Stack (L-232).java
@@ -0,0 +1,44 @@
+class MyQueue {
+    
+    Stack<Integer> stack1,stack2;
+
+    public MyQueue() {
+        stack1 = new Stack();
+        stack2 = new Stack();
+    }
+    
+    public void push(int x) {
+        stack1.push(x);
+        
+    }
+    
+    public int pop() {
+        while(stack1.size()>1){
+            stack2.push(stack1.pop());
+        }
+        int data = stack1.pop();
+        while(!stack2.isEmpty()){
+            stack1.push(stack2.pop());
+        }
+        return data;
+    }
+    
+    public int peek() {
+        while(stack1.size()>1){
+            stack2.push(stack1.pop());
+        }
+        int data = stack1.peek();
+        while(!stack2.isEmpty()){
+            stack1.push(stack2.pop());
+        }
+        return data;
+        
+    }
+    
+    public boolean empty() {
+        return stack1.size()== 0;
+        
+    }
+}
+
+ 
